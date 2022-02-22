6770e09ae0e5a0cd5c5c1107153e15d94cc97e38
Amna <amna.bashir2@gmail.com>
2022-02-22 18:54:44 +0000

    created files table

--- src/main/java/com/codeclan/example/folderManagement/models/File.java
+++ src/main/java/com/codeclan/example/folderManagement/models/File.java
@@ -1,5 +1,7 @@
 package com.codeclan.example.folderManagement.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
@@ -16,7 +18,60 @@ public class File {
     private String extension;
 
     @Column(name = "size")
-    private int size;
+    private String size;
 
-    @ManyToOne()
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    @JsonIgnoreProperties({"files"})
+    private Folder folder;
+
+    public File(String name, String extension, String size, Folder folder) {
+        this.name = name;
+        this.extension = extension;
+        this.size = size;
+        this.folder = folder;
+    }
+
+    public File() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
 }
--- src/test/java/com/codeclan/example/folderManagement/FolderManagementApplicationTests.java
+++ src/test/java/com/codeclan/example/folderManagement/FolderManagementApplicationTests.java
@@ -1,13 +1,36 @@
 package com.codeclan.example.folderManagement;
 
+import com.codeclan.example.folderManagement.models.File;
+import com.codeclan.example.folderManagement.models.Folder;
+import com.codeclan.example.folderManagement.repositories.FileRepository;
+import com.codeclan.example.folderManagement.repositories.FolderRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class FolderManagementApplicationTests {
 
+	@Autowired
+	FolderRepository folderRepository;
+
+	@Autowired
+	FileRepository fileRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createFilesAndFolders(){
+		Folder folder = new Folder("Payment_Record");
+		folderRepository.save(folder);
+
+		File file = new File("Feb_payment","PDF", "30MB", folder);
+		fileRepository.save(file);
 	}
 
 }
